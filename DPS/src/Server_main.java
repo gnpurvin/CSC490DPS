67df9da9a23638790773a23d765664892acd48fd
Ray Cockerham <raytard37.5@gmail.com>
2019-03-28 13:08:49 -0400

    Committing connectivity to master
    added functionality for dice roller implementation.

---
+++ DPS/src/Server_main.java
@@ -0,0 +1,22 @@
+package Connectivity;
+
+/**
+ * Test class that is part of the Dungeon Positioning System software created for a senior project at UNCG by
+ * Phil, Liz, Spencer, Greg, and Ray.
+ * 
+ */
+import java.io.*;
+
+class Server_Main {
+
+   
+    public static void main(String[] args) throws IOException {
+
+        int servPort = 11064;
+        Server server = new Server(servPort);
+        System.out.println("Server Online.");
+        server.start();
+
+    }
+
+}
