accddf7be75e1f8f199476be2703eb18d212e36a
gnpurvin <gnpurvin@uncg.edu>
2019-03-28 12:03:45 -0400

    changed DiceRoller for integration with connectivity functionality

--- DPS/src/qualityoflife/DiceRoller.java
+++ DPS/src/qualityoflife/DiceRoller.java
@@ -4,6 +4,7 @@
 package qualityoflife;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * @author Greg
@@ -16,13 +17,20 @@ public class DiceRoller {
 	/** highest number on the die */
 	private int size;
 	
+	/** number of dice to roll */
+	private int numDice;
+	
 	private static final int[] SIZES = new int[] {4, 6, 8, 10, 12, 20, 100};
 
 	/**
 	 * constructor for the dice roller
 	 * @param size of the dice being rolled
 	 */
-	public DiceRoller(int size) {
+	public DiceRoller(String input) {
+		Scanner inScan = new Scanner(input.toLowerCase());
+		inScan.useDelimiter("d");
+		int num = inScan.nextInt();
+		int size = inScan.nextInt();
 		boolean valid = false;
 		for(int i = 0; i < SIZES.length; i++) {
 			if(size == SIZES[i]) {
@@ -34,14 +42,27 @@ public class DiceRoller {
 			throw new IllegalArgumentException("Invalid Die Size.");
 		}
 		this.size = size;
+		this.numDice = num;
 	}
 	
+	public int getSize() {
+		return size;
+	}
+
+	public int getNumDice() {
+		return numDice;
+	}
+
+	public static int[] getSizes() {
+		return SIZES;
+	}
+
 	/**
 	 * Rolls a number of dice according to the given parameter
 	 * @param numDice the number of dice being rolled
 	 * @return string with the result of the roll
 	 */
-	public String roll(int numDice) {
+	public String roll() {
 		String result = "rolled a ";
 		Random rand = new Random();
 		int sum = 0;
@@ -51,14 +72,14 @@ public class DiceRoller {
 		result += sum;
 		return result;
 	}
-	
-	/**
-	 * rolls a single die of the given size
-	 * @return result of the roll
-	 */
-	public int roll() {
-		Random rand = new Random();
-		return rand.nextInt(size) + 1; 
-	}
+//	
+//	/**
+//	 * rolls a single die of the given size
+//	 * @return result of the roll
+//	 */
+//	public int roll() {
+//		Random rand = new Random();
+//		return rand.nextInt(size) + 1; 
+//	}
 
 }
