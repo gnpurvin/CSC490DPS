6026a7ceb6bccc915befeec15ddfaa363bf33f8f
FiliusRomanus <filiusromanus357@gmail.com>
2019-04-11 14:51:18 -0400

    Update DPS.java

--- DPS/src/dps/DPS.java
+++ DPS/src/dps/DPS.java
@@ -6,11 +6,9 @@
 package dps;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -24,7 +22,7 @@ public class DPS extends Application {
     
     StackPane mapping = mpc.mapping;
     Scene mapScene = mpc.mapScene;
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/GUI/MainMenu.fxml"));
