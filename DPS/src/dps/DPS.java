7fffc53bbc596e7970ce53f6554be2428a5d617a
Phillip Jones <filiusromanus357@gmail.com>
2019-04-11 16:39:30 -0400

    halp

--- DPS/src/dps/DPS.java
+++ DPS/src/dps/DPS.java
@@ -5,50 +5,36 @@
  */
 package dps;
 
-import GUI.LoginController;
-import database.connector;
-import java.sql.Connection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import javafx.scene.layout.StackPane;
-
-import javafx.scene.layout.AnchorPane;
-
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 /**
  *
  * @author Spencer
  */
 public class DPS extends Application {
-
     mapController mpc = new mapController();
     StackPane mapping = mpc.mapping;
     Scene mapScene = mpc.mapScene;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/MainMenu.fxml"));
+        primaryStage.setTitle("DPS");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Connection con = connector.connect();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/Login.fxml"));
-        LoginController controller = new LoginController(con);        
-        loader.setController(controller);
-        Parent root = (AnchorPane) loader.load();
-        primaryStage.setTitle("DPS");
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
-    }
-
+        
+}
 
 }
