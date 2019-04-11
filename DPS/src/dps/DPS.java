a1e4bb9df6e9adba614968eea7bf74d020c6c9cb
Phillip Jones <filiusromanus357@gmail.com>
2019-04-11 14:48:22 -0400

    fhajklsdhjadsklhfas

--- DPS/src/dps/DPS.java
+++ DPS/src/dps/DPS.java
@@ -6,9 +6,9 @@
 package dps;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -19,31 +19,11 @@ public class DPS extends Application {
     
     
     
-     
     @Override
-    public void start(Stage primaryStage) {
-        mapController mpc = new mapController();
-        StackPane mapping = mpc.mapping;
-        Scene mapScene = mpc.mapScene;
-    
-        Button btn1 = new Button();
-        btn1.setText("Say 'Hello World'");
-        
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 300, 250);
-        root.getChildren().add(btn1);
-        
-        mpc.drawMap();
-        
-        Button btn2 = new Button();
-        btn2.setText("Make map");
-        mapping.getChildren().add(btn2);
-        btn2.setOnAction(e -> mapping.getChildren().add(mpc.c));
-        
-        btn1.setOnAction(e -> primaryStage.setScene(mapScene));
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/MainMenu.fxml"));
+        primaryStage.setTitle("DPS");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
